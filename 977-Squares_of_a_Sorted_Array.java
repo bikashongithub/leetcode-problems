860747572f40d6c0212182dc73848fb92388daba
Bikash Mishra <slashdotbikash@gmail.com>
2022-06-27 19:44:56 +0530

    977. Squares of a Sorted Array

---
+++ 977-Squares_of_a_Sorted_Array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int square;
+        for (int i = 0; i < nums.length; i++) {
+            square = nums[i] * nums[i];
+            nums[i] = square;
+        }
+
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    // swap nums[j+1] and nums[j]
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+
+    }
+}
+
+// solution 2
+/*
+ *
+ * for(int i=0; i<nums.length; i++) {
+ * nums[i] = nums[i]*nums[i];
+ * }
+ *
+ * Arrays.sort(nums);
+ * return nums;
+ *
+ */
