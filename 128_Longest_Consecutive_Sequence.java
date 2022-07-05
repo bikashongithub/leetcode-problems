daa3f778dd78d1d6cc6999d8470f6cbbd597175d
Bikash Mishra <slashdotbikash@gmail.com>
2022-07-05 16:53:16 +0530

    longest consequtive sequence

--- 128_Longest_Consecutive_Sequence.java
+++ 128_Longest_Consecutive_Sequence.java
@@ -13,14 +13,17 @@ class Solution {
         // iterate every items in nums array
         for (int item : nums) {
 
-            // check if there is item smaller by just one than the current item present in
+            // check if there is item smaller by just one than the current item present in ..
             // hashset or not(basically checking the start point of the sequence)
             if (!set.contains(item - 1)) {
                 int tl = 1;
+
+                // if not present then keep increasing the tl(total length of ongoing sequence)
                 while (set.contains(item + tl)) {
                     tl++;
                 }
 
+                // compare with previous maxLength of sequence
                 if (maxLength < tl) {
                     maxLength = tl;
 
