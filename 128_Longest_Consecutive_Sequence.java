f553318549fc390e00c39748575b74c03e9bf83b
Bikash Mishra <slashdotbikash@gmail.com>
2022-07-05 16:50:07 +0530

    candies

---
+++ 128_Longest_Consecutive_Sequence.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int maxLength = 0;
+
+        // create a hashset and add all the elements of nums array into hash set
+        HashSet<Integer> set = new HashSet<>();
+        for (int item : nums) {
+            set.add(item);
+        }
+
+        // iterate every items in nums array
+        for (int item : nums) {
+
+            // check if there is item smaller by just one than the current item present in
+            // hashset or not(basically checking the start point of the sequence)
+            if (!set.contains(item - 1)) {
+                int tl = 1;
+                while (set.contains(item + tl)) {
+                    tl++;
+                }
+
+                if (maxLength < tl) {
+                    maxLength = tl;
+
+                }
+            }
+        }
+        return maxLength;
+
+    }
+}
